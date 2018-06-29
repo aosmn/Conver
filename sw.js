c3355ad8ba52fd6569fc527eafe7201bbf94f9f8
Alaa Osman <alaa.osmana@gmail.com>
2018-06-30 02:10:05 +0300

    fixing cache issue

--- sw.js
+++ sw.js
@@ -48,10 +48,3 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
-
-
-self.addEventListener('message', function(event) {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
-});
